8140fe71ec6867fefcc7282f99ee4c5f30035dec
arseny <arsenykonohov@gmail.com>
2015-09-28 23:09:40 +0300

    slider done

--- _build/_dev/_script/3_blocks/scroltoid.js
+++ _build/_dev/_script/3_blocks/scroltoid.js
@@ -3,45 +3,45 @@ var jQuery,
 (function ($) {
     "use strict";
 
-    var offer = $('#offer');
-
+    var offer = $('#offer__id'),
+        principle = $('#principle_id'),
+        office = $('#office_id');
+    
+    
+    
     var waypoint_offer = new Waypoint({
-        element: document.getElementById('offer'),
+        element: document.getElementById('offer__id'),
         handler: function(direction) {
             offer.css('display','none');
             setTimeout(function(){
                 offer.css('display','inline-block');
             },10);
         },
-        offset: '40%'
+        offset: '80%'
     });
-
-    var principle = $('#principle');
-
+    
     var waypoint_principle = new Waypoint({
-        element: document.getElementById('principle'),
+        element: document.getElementById('principle_id'),
         handler: function(direction) {
             principle.css('display','none');
             setTimeout(function(){
                 principle.css('display','inline-block');
             },10);
         },
-        offset: '40%'
+        offset: '80%'
     });
-
-    var office = $('#office');
-
+    
     var waypoint_office = new Waypoint({
-        element: document.getElementById('office'),
+        element: document.getElementById('office_id'),
         handler: function(direction) {
             office.css('display','none');
             setTimeout(function(){
                 office.css('display','inline-block');
             },10);
         },
-        offset: '40%'
+        offset: '80%'
     });
-
-
+    
+    
         console.log("scrolltoid.js");
 }(jQuery));
