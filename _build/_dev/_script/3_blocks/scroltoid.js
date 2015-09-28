974f2492880375d76b09fc9af9ce6a6dea1c5095
arseny <arsenykonohov@gmail.com>
2015-09-28 19:47:46 +0300

    scrollltoid

--- _build/_dev/_script/3_blocks/scroltoid.js
+++ _build/_dev/_script/3_blocks/scroltoid.js
@@ -2,12 +2,46 @@ var jQuery,
     $ = jQuery;
 (function ($) {
     "use strict";
-    
-    
-    
-    
-    
-    
-    
-    console.log("scrolltoid.js");
+
+    var offer = $('#offer');
+
+    var waypoint_offer = new Waypoint({
+        element: document.getElementById('offer'),
+        handler: function(direction) {
+            offer.css('display','none');
+            setTimeout(function(){
+                offer.css('display','inline-block');
+            },10);
+        },
+        offset: '40%'
+    });
+
+    var principle = $('#principle');
+
+    var waypoint_principle = new Waypoint({
+        element: document.getElementById('principle'),
+        handler: function(direction) {
+            principle.css('display','none');
+            setTimeout(function(){
+                principle.css('display','inline-block');
+            },10);
+        },
+        offset: '40%'
+    });
+
+    var office = $('#office');
+
+    var waypoint_office = new Waypoint({
+        element: document.getElementById('office'),
+        handler: function(direction) {
+            office.css('display','none');
+            setTimeout(function(){
+                office.css('display','inline-block');
+            },10);
+        },
+        offset: '40%'
+    });
+
+
+        console.log("scrolltoid.js");
 }(jQuery));
