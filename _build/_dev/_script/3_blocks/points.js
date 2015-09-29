5c3405f173b1487ec2f4b54652f6bcb85753eb65
AnhieL89 <Shershnev.dn@gmail.com>
2015-09-29 15:07:16 +0300

    active nav dottes

--- _build/_dev/_script/3_blocks/points.js
+++ _build/_dev/_script/3_blocks/points.js
@@ -3,11 +3,33 @@ var jQuery,
 (function ($) {
     "use strict";
 
+    var nav = $('.nav__item');
+
     $('.page__line').waypoint(function(direction){
-        var selector = $(this).find('a.nav__anchor').attr('class');
+        var selector = $(this.options.element).find('a.nav__anchor').attr('name');
         console.log(selector);
-        $('.nav__item').find('a[href="#' + selector + '"]').addClass('active');
+        nav.find('a').removeClass('nav__link_act');
+        nav.find('a[href="#' + selector + '"]').addClass('nav__link_act');
     });
-    
+
+    $(window).scroll( function(){
+
+        if ($(window).scrollTop() == 0) {
+            nav.find('a').removeClass('nav__link_act');
+            $('.nav__item:first-child').find('a').addClass('nav__link_act');
+        }
+
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            nav.find('a').removeClass('nav__link_act');
+            $('.nav__item:last-child').find('a').addClass('nav__link_act');
+        }
+
+    });
+
+    nav.on('click',function(){
+        nav.find('a').removeClass('nav__link_act');
+        $(this).find('a').addClass('nav__link_act');
+    });
+
     console.log("points.js");
 }(jQuery));
